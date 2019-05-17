d24def344f146b056f19c3dd05e0ff3b14cf0322
Joel David <jdavidhc1710@gmail.com>
2019-05-17 00:13:28 -0400

    main

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from src.database import *
 from src.defuzzification import *
 from src.fuzzification import *
+from src.utilis import category_selector as cs
+
 
 class Personal_buyer:
     def __init__(self, micro, videoCard, screen, price):
-        self.micro = micro
-        self.videoCard= videoCard
-        self.screen = screen
-        self.price = price
+        self.micro = cs(micro, "micro")
+        self.videoCard = cs(videoCard, "videoCard")
+        self.screen = cs(screen, "screen")
+        self.price = cs(price, "price")

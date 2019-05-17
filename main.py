d7855907bf31cdfce740774237ba26d671aed228
Joel David <jdavidhc1710@gmail.com>
2019-05-17 10:14:15 -0400

    Update gitignore, and star to work on IF-THEN rules

--- main.py
+++ main.py
@@ -6,7 +6,22 @@ from src.utilis import category_selector as cs
 
 class Personal_buyer:
     def __init__(self, micro, videoCard, screen, price):
-        self.micro = cs(micro, "micro")
-        self.videoCard = cs(videoCard, "videoCard")
-        self.screen = cs(screen, "screen")
-        self.price = cs(price, "price")
+        self.micro = cs(micro)
+        self.videoCard = cs(videoCard)
+        self.screen = cs(screen)
+        self.price = cs(price)
+
+    def __str__(self):
+        return "micro.value: {} - micro.category: {}\n" \
+               "videoCard.value: {} - videoCard.category: {}\n" \
+               "screen.value: {} - screen.category: {}\n" \
+               "price.value: {} - price.category: {}\n".format(self.micro["value"], self.micro["category"],
+                                                               self.videoCard["value"], self.videoCard["category"],
+                                                               self.screen["value"], self.screen["category"],
+                                                               self.price["value"], self.price["category"])
+
+
+if __name__ == "__main__":
+    assistant = Personal_buyer(35, 20, 10, 15)
+
+    print(assistant)
--- src/utilis.py
+++ src/utilis.py
@@ -1,17 +1,17 @@
 from src.database import *
 
 
-def category_selector(x, obj):
+def category_selector(x):
     """
     :param x:
     :param obj:
     :return:    dict: fuzzy value: category
     """
     if bad(x) > 0:
-        return {"value": bad(x), "category": obj + "_bad"}
+        return {"value": bad(x), "category": "bad"}
     elif regular(x) > 0:
-        return {"value": regular(x), "category": obj + "_regular"}
+        return {"value": regular(x), "category": "regular"}
     elif good(x) > 0:
-        return {"value": good(x), "category": obj + "_good"}
+        return {"value": good(x), "category": "good"}
     else:
-        return {"value": veryGood(x), "category": obj + "_veryGood"}
+        return {"value": veryGood(x), "category": "veryGood"}
